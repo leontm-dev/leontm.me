5b9e0f6dc3dce6b038d4c9a5a090b98148d17a58
Leon™ <leontm.dev@gmail.com>
2023-10-21 19:09:36 +0200

    Added general packages and server configurations

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 // Imports
+
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+
+// Settings
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    origin: "*",
+  })
+);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.listen(2000, () => {
+  console.log("Server running on port 2000");
+});
+
+// Routes
