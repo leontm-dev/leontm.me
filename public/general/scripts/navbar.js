379121ef212d12e20aa0a259036f77ae9f7cd750
Leon™ <leontm.dev@gmail.com>
2023-10-23 15:26:50 +0200

    Added a general navbar script for the general navbar

---
+++ public/general/scripts/navbar.js
@@ -0,0 +1,14 @@
+const url = "";
+document.getElementById("navbar-home").addEventListener("click", (ev) => {
+  window.location.href = `${url}`;
+});
+document.getElementById("navbar-about").addEventListener("click", (ev) => {
+  window.location.href = `${url}/about`;
+});
+
+document.getElementById("navbar-apps").addEventListener("click", (ev) => {
+  window.location.href = `${url}/app-store`;
+});
+document.getElementById("navbar-projects").addEventListener("click", (ev) => {
+  window.location.href = `${url}/projects`;
+});
