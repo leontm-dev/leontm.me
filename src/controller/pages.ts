c1b819f956d32ef299bb5558804cddb60c36e3a9
LeonTM <lanneken09@gmail.com>
2024-03-22 19:07:02 +0100

    Added a controller for the pages

---
+++ src/controller/pages.ts
@@ -0,0 +1,16 @@
+// Imports
+
+import express from "express";
+
+// Code
+
+const openHomePage = (req: express.Request, res: express.Response) => {
+  res.render("home");
+};
+const openAboutPage = (req: express.Request, res: express.Response) => {
+  res.render("about");
+};
+
+// Exports
+
+export default { openHomePage, openAboutPage };
