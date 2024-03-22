45498587a0aa61b326280c0752a9ce5ddc806faa
LeonTM <lanneken09@gmail.com>
2024-03-22 19:04:34 +0100

    Created the main router file for the pages

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+// Imports
+
+import express from "express";
+
+// Project-Imports
+
+// Presets
+
+const router = express.Router();
+
+// Code
+
+export default (): express.Router => {
+  return router;
+};
