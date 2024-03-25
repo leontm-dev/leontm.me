5f0d9045cf08242e2ab46880d86117809652aff1
LeonTM <lanneken09@gmail.com>
2024-03-25 18:49:15 +0100

    Ran build

--- dist/controller/pages.js
+++ dist/controller/pages.js
@@ -1,7 +1,7 @@
 "use strict";
 // Imports
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.openAboutPage = exports.openHomePage = void 0;
+exports.openAppStorePage = exports.openRegisterPage = exports.openLoginPage = exports.openProjectsPage = exports.openAccountPage = exports.openAboutPage = exports.openHomePage = void 0;
 // Code
 const openHomePage = (req, res) => {
     res.render("home");
@@ -11,3 +11,28 @@ const openAboutPage = (req, res) => {
     res.render("about");
 };
 exports.openAboutPage = openAboutPage;
+const openAccountPage = (req, res) => {
+    if (req.cookies.userToken) {
+        res.render("account");
+    }
+    else {
+        res.redirect("login");
+    }
+};
+exports.openAccountPage = openAccountPage;
+const openProjectsPage = (req, res) => {
+    res.render("projects");
+};
+exports.openProjectsPage = openProjectsPage;
+const openLoginPage = (req, res) => {
+    res.render("login");
+};
+exports.openLoginPage = openLoginPage;
+const openRegisterPage = (req, res) => {
+    res.render("signup");
+};
+exports.openRegisterPage = openRegisterPage;
+const openAppStorePage = (req, res) => {
+    res.render("app-store");
+};
+exports.openAppStorePage = openAppStorePage;
