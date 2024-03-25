4990bfd929c69f2d0fe92f42c1c225f4589ff01e
LeonTM <lanneken09@gmail.com>
2024-03-25 18:46:08 +0100

    Added new methods

--- src/controller/pages.ts
+++ src/controller/pages.ts
@@ -10,6 +10,25 @@ const openHomePage = (req: express.Request, res: express.Response) => {
 const openAboutPage = (req: express.Request, res: express.Response) => {
   res.render("about");
 };
+const openAccountPage = (req: express.Request, res: express.Response) => {
+  if (req.cookies.userToken) {
+    res.render("account");
+  } else {
+    res.redirect("login");
+  }
+};
+const openProjectsPage = (req: express.Request, res: express.Response) => {
+  res.render("projects");
+};
+const openLoginPage = (req: express.Request, res: express.Response) => {
+  res.render("login");
+};
+const openRegisterPage = (req: express.Request, res: express.Response) => {
+  res.render("signup");
+};
+const openAppStorePage = (req: express.Request, res: express.Response) => {
+  res.render("app-store");
+};
 
 // Exports
 
