4106e548b7a7500030eb08648a306954d3557bfb
LeonTM <lanneken09@gmail.com>
2024-03-25 19:24:03 +0100

    Made this code more efficient

--- src/helper/sendApiResponse.ts
+++ src/helper/sendApiResponse.ts
@@ -5,46 +5,29 @@ import { ApiResponse } from "../types/apiResponse";
 // Code
 
 const sendApiResponse = (status: number, content: any): ApiResponse => {
+  const messages: { [key: number]: string } = {
+    200: "Success",
+    201: "Created",
+    204: "No Content",
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+    409: "Conflict",
+    500: "Internal Server Error",
+  };
+
+  const message = messages[status] || "Unknown";
+
   const response: ApiResponse = {
     status,
-    message: "",
+    message,
     data: content,
   };
-  switch (status) {
-    case 200:
-      response.message = "Success";
-      break;
-    case 201:
-      response.message = "Created";
-      break;
-    case 204:
-      response.message = "No Content";
-      break;
-    case 400:
-      response.message = "Bad Request";
-      break;
-    case 401:
-      response.message = "Unauthorized";
-      break;
-    case 403:
-      response.message = "Forbidden";
-      break;
-    case 404:
-      response.message = "Not Found";
-      break;
-    case 409:
-      response.message = "Conflict";
-      break;
-    case 500:
-      response.message = "Internal Server Error";
-      break;
-    default:
-      response.message = "Unknown";
-      break;
-  }
+
   return response;
 };
 
 // Exports
 
-export default sendApiResponse;
+export { sendApiResponse };
