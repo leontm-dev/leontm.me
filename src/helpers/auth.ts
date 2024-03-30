c5bc619dca4a0d71303ee7da14aca9b32da44c79
LeonTM <lanneken09@gmail.com>
2024-03-30 19:01:50 +0100

    Created a helper for auth

---
+++ src/helpers/auth.ts
@@ -0,0 +1,13 @@
+// Imports
+
+import crypto from "crypto";
+
+// Code
+
+const random = () => crypto.randomBytes(128).toString("base64");
+const auth = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha512", [salt, password].join("/"))
+    .update(<string>process.env.GLOBAL_SECRET)
+    .digest("hex");
+};
