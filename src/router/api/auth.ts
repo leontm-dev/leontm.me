4623f806d78b543c8fa71af19512a55ee2cb206e
LeonTM <lanneken09@gmail.com>
2024-03-30 19:26:26 +0100

    Added content to the router

--- src/router/api/auth.ts
+++ src/router/api/auth.ts
@@ -4,6 +4,12 @@ import express from "express";
 
 // Project-Imports
 
+import { register, login, logout } from "../../controller/auth";
+
 // Code
 
-export default (router: express.Router) => {};
+export default (router: express.Router) => {
+  router.post("/api/auth/register", register);
+  router.post("/api/auth/login", login);
+  router.post("/api/auth/logout", logout);
+};
