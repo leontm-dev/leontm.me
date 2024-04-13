5c05352bf7e528d6693543b21c9ca351f29ffd40
LeonTM <lanneken09@gmail.com>
2024-04-13 19:05:48 +0200

    Fixed ts error

--- src/projects/BetterTrackerExtension/controller/smurf.ts
+++ src/projects/BetterTrackerExtension/controller/smurf.ts
@@ -12,18 +12,31 @@ import sendApiResponse from "../../../helpers/sendApiResponse";
 const createSmurf = async (req: express.Request, res: express.Response) => {
   try {
     if (!req.body)
-      return res.status(400).json(sendApiResponse(400, null)).end();
+      return res
+        .status(400)
+        .json(sendApiResponse(400, null, "Body is necessary."))
+        .end();
 
     const toBeCreated = req.body;
     delete toBeCreated.url;
 
     const smurf = await create(req.body.url, toBeCreated);
-    if (!smurf) return res.status(400).json(sendApiResponse(400, null)).end();
+    if (!smurf)
+      return res
+        .status(400)
+        .json(sendApiResponse(400, null, "Check the body once more."))
+        .end();
 
-    return res.status(204).json(sendApiResponse(204, null)).end();
+    return res
+      .status(204)
+      .json(sendApiResponse(204, null, "Smurf created."))
+      .end();
   } catch (error) {
     console.error(error);
-    return res.status(500).json(sendApiResponse(500, null)).end();
+    return res
+      .status(500)
+      .json(sendApiResponse(500, null, "Internal Server Error, our bad."))
+      .end();
   }
 };
 
