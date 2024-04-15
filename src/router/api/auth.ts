6bfa96a8e4dd0f9d5e76cd7c7ae79a2de1c08843
LeonTM <lanneken09@gmail.com>
2024-04-15 11:20:44 +0200

    Removed logout route

--- src/router/api/auth.ts
+++ src/router/api/auth.ts
@@ -4,12 +4,11 @@ import express from "express";
 
 // Project-Imports
 
-import { register, login, logout } from "../../controller/auth";
+import { register, login } from "../../controller/auth";
 
 // Code
 
 export default (router: express.Router) => {
   router.post("/api/auth/register", register);
   router.post("/api/auth/login", login);
-  router.post("/api/auth/logout", logout);
 };
