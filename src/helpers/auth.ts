770e7649fd6ab040e26730a2e29931cdd7a63337
LeonTM <lanneken09@gmail.com>
2024-04-15 17:33:19 +0200

    Updated the auth function

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,13 +1,16 @@
 // Imports
 
 import crypto from "crypto";
+import env from "dotenv";
 
 // Code
+
+env.config({ debug: true });
 
 const random = () => crypto.randomBytes(128).toString("base64");
 const auth = (salt: string, password: string) => {
   return crypto
-    .createHmac("sha512", [salt, password].join("/"))
+    .createHmac("sha256", <string>[salt, password].join("/"))
     .update(<string>process.env.GLOBAL_SECRET)
     .digest("hex");
 };
