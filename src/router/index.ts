74dd8e2af79bf3d589ebc77e7b229e1e25379622
LeonTM <lanneken09@gmail.com>
2024-04-15 16:40:29 +0200

    Updated the main router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,15 @@
 import express from "express";
 
 // Project-Imports
+// API
+
+import auth from "./api/auth";
+import user from "./api/user";
+import test from "./api/test";
+
+// Projects
+
+import betterTracker from "./api/projects/betterTracker";
 
 // Presets
 
@@ -11,5 +20,11 @@ const router = express.Router();
 // Code
 
 export default (): express.Router => {
+  // API
+  auth(router);
+  user(router);
+  test(router);
+  // Projects
+  betterTracker(router);
   return router;
 };
