5564e36315d96b4e5172a9c348a45bbdb5eb70fe
LeonTM <lanneken09@gmail.com>
2024-04-22 16:57:07 +0200

    Made this router independent

--- src/router/pages.ts
+++ src/router/pages.ts
@@ -17,7 +17,9 @@ import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 // Code
 
-export default (router: express.Router) => {
+const router = express.Router();
+
+export default (): express.Router => {
   router.get("/", openHomePage);
   router.get("/about", openAboutPage);
   router.get("/account", isAuthenticated, openAccountPage);
@@ -25,4 +27,5 @@ export default (router: express.Router) => {
   router.get("/app-store", openAppStorePage);
   router.get("/projects", openProjectsPage);
   router.get("/register", openRegisterPage);
+  return router;
 };
