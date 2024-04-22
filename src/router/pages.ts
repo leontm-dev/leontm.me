88ae8749d5146813bf5ee5f9461300b4b55715c1
LeonTM <lanneken09@gmail.com>
2024-04-22 16:50:05 +0200

    Trying to add more routes

--- src/router/pages.ts
+++ src/router/pages.ts
@@ -4,11 +4,25 @@ import express from "express";
 
 // Project-Imports
 
-import { openHomePage, openAboutPage } from "../controller/pages";
+import {
+  openHomePage,
+  openAboutPage,
+  openAccountPage,
+  openLoginPage,
+  openAppStorePage,
+  openProjectsPage,
+  openRegisterPage,
+} from "../controller/pages";
+import {} from "../middlewares/isAuthenticated";
 
 // Code
 
 export default (router: express.Router) => {
   router.get("/", openHomePage);
   router.get("/about", openAboutPage);
+  router.get("/account", openAccountPage);
+  router.get("/login", openLoginPage);
+  router.get("/app-store", openAppStorePage);
+  router.get("/projects", openProjectsPage);
+  router.get("/register", openRegisterPage);
 };
