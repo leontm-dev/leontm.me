8e86588ae5733d6ccfe3ecd7d8fd1ee2a4b9c846
LeonTM <lanneken09@gmail.com>
2024-05-08 17:10:44 +0200

    Created a new script for the signup page

---
+++ dist/pages/public/login/signup.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("form").style.display = "flex";
+  }, 1000);
+};
