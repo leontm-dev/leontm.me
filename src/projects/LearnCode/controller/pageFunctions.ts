910bbfd6d4a02025dbca227fc53bad959fad0884
LeonTM <lanneken09@gmail.com>
2024-05-17 15:27:01 +0200

    Created controller file

---
+++ src/projects/LearnCode/controller/pageFunctions.ts
@@ -0,0 +1,14 @@
+// Imports
+
+import express from "express";
+
+// Project-Imports
+
+import sendApiResponse from "../../../helpers/sendApiResponse";
+
+// Code
+
+const giveDownloadFunction = (
+  req: express.Request,
+  res: express.Response
+) => {};
