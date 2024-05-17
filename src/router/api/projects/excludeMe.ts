43ffa498efaea1221514ab5a0ab136fd26e6c7ab
LeonTM <lanneken09@gmail.com>
2024-05-17 15:25:47 +0200

    Updated the router

--- src/router/api/projects/excludeMe.ts
+++ src/router/api/projects/excludeMe.ts
@@ -4,12 +4,69 @@ import express from "express";
 
 // Project-Imports
 
+import sendApiResponse from "../../../helpers/sendApiResponse";
+import {
+  createCollection,
+  getAllCollections,
+  getCollectionById,
+  getCollectionByName,
+  updateCollectionById,
+  updateCollectionByName,
+  deleteCollectionById,
+  deleteCollectionByName,
+} from "../../../projects/ExcludeMe/controller/collection";
+
 // Code
 
 export default (router: express.Router) => {
-  router.post("/projects/excludeMe/collection/create");
-  router.put("/projects/excludeMe/collection/update");
-  router.delete("/projects/excludeMe/collection/delete");
-  router.get("/projects/excludeMe/collection/get");
-  router.get("/projects/excludeMe/collection/getAll");
+  router.post("/projects/excludeMe/collection/create", createCollection);
+  router.put("/projects/excludeMe/collection/update/:type", (req, res) => {
+    if (req.params.type === "id") {
+      return updateCollectionById(req, res);
+    } else if (req.params.type === "name") {
+      return updateCollectionByName(req, res);
+    } else {
+      return res
+        .status(400)
+        .json(sendApiResponse(400, null, "Invalid type"))
+        .end();
+    }
+  });
+  router.delete("/projects/excludeMe/collection/delete/:type", (req, res) => {
+    if (req.params.type === "id") {
+      return deleteCollectionById(req, res);
+    } else if (req.params.type === "name") {
+      return deleteCollectionByName(req, res);
+    } else {
+      return res
+        .status(400)
+        .json(sendApiResponse(400, null, "Invalid type"))
+        .end();
+    }
+  });
+  router.get("/projects/excludeMe/collection/get/:type", (req, res) => {
+    if (req.params.type === "id") {
+      return getCollectionById(req, res);
+    } else if (req.params.type === "name") {
+      return getCollectionByName(req, res);
+    } else {
+      return res
+        .status(400)
+        .json(sendApiResponse(400, null, "Invalid type"))
+        .end();
+    }
+  });
+  router.get("/projects/excludeMe/collection/getAll", getAllCollections);
+  router.patch("/projects/excludeMe/collection/update/:type", (req, res) => {
+    if (req.params.type === "id") {
+      return updateCollectionById(req, res);
+    } else if (req.params.type === "name") {
+      return updateCollectionByName(req, res);
+    } else {
+      return res
+        .status(400)
+        .json(sendApiResponse(400, null, "Invalid type"))
+        .end();
+    }
+  });
 };
