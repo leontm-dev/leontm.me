c1e9e7a6c02d20a80fb4cd270ccb56576a971389
LeonTM <lanneken09@gmail.com>
2024-05-24 16:29:03 +0200

    Updated the validateRequest function

--- src/helpers/validateReq.ts
+++ src/helpers/validateReq.ts
@@ -7,12 +7,29 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const validateReq = (req: express.Request, tier: 1): boolean => {
-  const result1 =
-    req.url.startsWith("https://leontm.me") ||
-    req.headers["leontm-auth"] == `DEV ${process.env.ADMIN_KEY}`;
-  const result = result1;
-  return result;
+const validateReq = (req: express.Request, tier: 1 | 2 | 3): boolean => {
+  let tier3 = false;
+  let tier2 = false;
+  let tier1 = false;
+  if (tier === 3) {
+    if (req.baseUrl === "leontm.me") {
+      tier3 = true;
+    }
+  } else if (tier === 2) {
+    if (req.headers.authorization === `DEV ${process.env.DEV_TOKEN}`) {
+      tier2 = true;
+    }
+  } else if (tier === 1) {
+  }
+  if (tier === 3 && tier3) {
+    return true;
+  } else if ((tier === 2 && tier2) || tier3) {
+    return true;
+  } else if ((tier === 1 && tier1) || (tier2 && tier2) || (tier3 && tier3)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Exports
