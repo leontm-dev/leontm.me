edd8434610a455b26c44aa3de188622efe0007df
LeonTM <lanneken09@gmail.com>
2024-07-04 19:50:50 +0200

    Added new functions to the user router

--- src/router/api/user.ts
+++ src/router/api/user.ts
@@ -4,6 +4,26 @@ import express from "express";
 
 // Project-Imports
 
+import {
+  getAllUsers,
+  getUserById,
+  getUserBySessionToken,
+  getUserByUsername,
+  deleteUser,
+  updateUserAuth,
+  updateUserInfo,
+  updateUserServices,
+} from "../../controller/user";
+
 // Code
 
-export default (router: express.Router) => {};
+export default (router: express.Router) => {
+  router.get("/users", getAllUsers);
+  router.get("/users/:id", getUserById);
+  router.get("/users/username/:username", getUserByUsername);
+  router.get("/users/session/:sessionToken", getUserBySessionToken);
+  router.put("/users/auth/:id", updateUserAuth);
+  router.put("/users/info/:id", updateUserInfo);
+  router.put("/users/services/:id", updateUserServices);
+  router.delete("/users/:id", deleteUser);
+};
