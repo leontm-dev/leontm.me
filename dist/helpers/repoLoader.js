ac96c7aa74c91cddd664007170889697ea27dbf1
LeonTM <lanneken09@gmail.com>
2024-07-04 12:04:45 +0200

    Updated the repo fullPath

--- dist/helpers/repoLoader.js
+++ dist/helpers/repoLoader.js
@@ -1,27 +1,5 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+// Imports
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -31,31 +9,86 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadRepo = void 0;
 const child_process_1 = require("child_process");
+const path_1 = __importDefault(require("path"));
+const fs_extra_1 = __importDefault(require("fs-extra"));
+// Code
 function loadRepo(repoUrl, repoPath) {
-    console.log(__dirname);
-    (0, child_process_1.exec)(`git clone ${repoUrl} ${repoPath}`, (error) => {
+    const fullPath = path_1.default.join("runnableProjects", repoPath);
+    // Prüfen, ob das Verzeichnis existiert
+    if (fs_extra_1.default.existsSync(fullPath)) {
+        // Aktuellen lokalen Commit-Hash abrufen
+        (0, child_process_1.exec)(`git -C ${fullPath} rev-parse HEAD`, (error, localHash) => {
+            if (error) {
+                console.error("Fehler beim Abrufen des lokalen Commit-Hash:", error);
+                return;
+            }
+            // Neuesten Remote-Commit-Hash abrufen
+            (0, child_process_1.exec)(`git ls-remote ${repoUrl} HEAD`, (error, remoteHash) => {
+                if (error) {
+                    console.error("Fehler beim Abrufen des Remote-Commit-Hash:", error);
+                    return;
+                }
+                if (localHash.split(" ")[0] !== remoteHash.split("\t")[0]) {
+                    console.log("Neuere Version des Repositories verfügbar. Aktualisiere...");
+                    updateRepo(fullPath);
+                }
+                else {
+                    console.log("Aktuellste Version bereits vorhanden.");
+                }
+            });
+        });
+    }
+    else {
+        console.log("Repository existiert nicht. Klone...");
+        cloneRepo(repoUrl, fullPath);
+    }
+}
+function cloneRepo(repoUrl, fullPath) {
+    (0, child_process_1.exec)(`git clone ${repoUrl} ${fullPath}`, (error) => {
         if (error) {
             console.error("Fehler beim Klonen des Repositories:", error);
             return;
         }
         console.log("Repository erfolgreich geklont.");
-        (0, child_process_1.exec)(`cd ${repoPath} && npm install`, (installError) => __awaiter(this, void 0, void 0, function* () {
+        (0, child_process_1.exec)(`cd ${fullPath} && npm install`, (installError) => __awaiter(this, void 0, void 0, function* () {
             if (installError) {
                 console.error("Fehler bei der Installation der Abhängigkeiten:", installError);
                 return;
             }
             console.log("Abhängigkeiten installiert.");
-            try {
-                const module = yield Promise.resolve(`${`${repoPath}/path/to/module.js`}`).then(s => __importStar(require(s)));
-                module.someFunction();
-            }
-            catch (importError) {
-                console.error("Fehler beim Importieren des Moduls:", importError);
-            }
+            runRepo(fullPath);
         }));
     });
 }
-exports.loadRepo = loadRepo;
+function updateRepo(fullPath) {
+    (0, child_process_1.exec)(`git -C ${fullPath} pull`, (error) => {
+        if (error) {
+            console.error("Fehler beim Aktualisieren des Repositories:", error);
+            return;
+        }
+        console.log("Repository erfolgreich aktualisiert.");
+        (0, child_process_1.exec)(`cd ${fullPath} && npm install`, (installError) => __awaiter(this, void 0, void 0, function* () {
+            if (installError) {
+                console.error("Fehler bei der Installation der Abhängigkeiten:", installError);
+                return;
+            }
+            console.log("Abhängigkeiten installiert.");
+            runRepo(fullPath);
+        }));
+    });
+}
+function runRepo(fullPath) {
+    try {
+        (0, child_process_1.exec)(`cd ${fullPath} && yarn run start`);
+    }
+    catch (importError) {
+        console.error("Fehler beim Starten des Projekts: ", importError);
+    }
+}
+// Exports
+exports.default = loadRepo;
--- src/helpers/repoLoader.ts
+++ src/helpers/repoLoader.ts
@@ -1,9 +1,48 @@
+// Imports
+
 import { exec } from "child_process";
 import path from "path";
+import fs from "fs-extra";
+
+// Code
 
 function loadRepo(repoUrl: string, repoPath: string) {
-  console.log(__dirname);
-  exec(`git clone ${repoUrl} ${repoPath}`, (error) => {
+  const fullPath = path.join("runnableProjects", repoPath);
+
+  // Prüfen, ob das Verzeichnis existiert
+  if (fs.existsSync(fullPath)) {
+    // Aktuellen lokalen Commit-Hash abrufen
+    exec(`git -C ${fullPath} rev-parse HEAD`, (error, localHash) => {
+      if (error) {
+        console.error("Fehler beim Abrufen des lokalen Commit-Hash:", error);
+        return;
+      }
+
+      // Neuesten Remote-Commit-Hash abrufen
+      exec(`git ls-remote ${repoUrl} HEAD`, (error, remoteHash) => {
+        if (error) {
+          console.error("Fehler beim Abrufen des Remote-Commit-Hash:", error);
+          return;
+        }
+
+        if (localHash.split(" ")[0] !== remoteHash.split("\t")[0]) {
+          console.log(
+            "Neuere Version des Repositories verfügbar. Aktualisiere..."
+          );
+          updateRepo(fullPath);
+        } else {
+          console.log("Aktuellste Version bereits vorhanden.");
+        }
+      });
+    });
+  } else {
+    console.log("Repository existiert nicht. Klone...");
+    cloneRepo(repoUrl, fullPath);
+  }
+}
+
+function cloneRepo(repoUrl: string, fullPath: string) {
+  exec(`git clone ${repoUrl} ${fullPath}`, (error) => {
     if (error) {
       console.error("Fehler beim Klonen des Repositories:", error);
       return;
@@ -11,7 +50,7 @@ function loadRepo(repoUrl: string, repoPath: string) {
 
     console.log("Repository erfolgreich geklont.");
 
-    exec(`cd ${repoPath} && npm install`, async (installError) => {
+    exec(`cd ${fullPath} && npm install`, async (installError) => {
       if (installError) {
         console.error(
           "Fehler bei der Installation der Abhängigkeiten:",
@@ -22,16 +61,43 @@ function loadRepo(repoUrl: string, repoPath: string) {
 
       console.log("Abhängigkeiten installiert.");
 
-      try {
-        const module = await import(`${repoPath}/path/to/module.js`);
-        module.someFunction();
-      } catch (importError) {
-        console.error("Fehler beim Importieren des Moduls:", importError);
-      }
+      runRepo(fullPath);
     });
   });
 }
 
+function updateRepo(fullPath: string) {
+  exec(`git -C ${fullPath} pull`, (error) => {
+    if (error) {
+      console.error("Fehler beim Aktualisieren des Repositories:", error);
+      return;
+    }
+
+    console.log("Repository erfolgreich aktualisiert.");
+
+    exec(`cd ${fullPath} && npm install`, async (installError) => {
+      if (installError) {
+        console.error(
+          "Fehler bei der Installation der Abhängigkeiten:",
+          installError
+        );
+        return;
+      }
+
+      console.log("Abhängigkeiten installiert.");
+
+      runRepo(fullPath);
+    });
+  });
+}
+function runRepo(fullPath: string) {
+  try {
+    exec(`cd ${fullPath} && yarn run start`);
+  } catch (importError) {
+    console.error("Fehler beim Starten des Projekts: ", importError);
+  }
+}
+
 // Exports
 
-export { loadRepo };
+export default loadRepo;
