df5feb31e7a9cd36fac6b5cf4d3b8429bb9ccdad
LeonTM <lanneken09@gmail.com>
2024-07-31 11:20:57 +0200

    unrequiring settings

--- dist/projects/EasyLogs/db/server.js
+++ dist/projects/EasyLogs/db/server.js
@@ -19,292 +19,234 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const settingsSchema = new mongoose_1.default.Schema({
     applicationCommandPermissionsUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     autoModerationActionExecution: {
         type: Boolean,
-        required: true,
         default: true,
     },
     autoModerationRuleCreate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     autoModerationRuleDelete: {
         type: Boolean,
-        required: true,
         default: true,
     },
     autoModerationRuleUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     channelCreate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     channelDelete: {
         type: Boolean,
-        required: true,
         default: true,
     },
     channelPinsUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     channelUpdate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     emojiCreate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     emojiDelete: {
         type: Boolean,
-        required: true,
         default: true,
     },
     emojiUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     entitlementCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     entitlementDelete: {
         type: Boolean,
-        required: true,
         default: false,
     },
     entitlementUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     guildAuditLogEntryCreate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildBanAdd: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildBanRemove: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildInteractionUpdate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildMemberAdd: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildMemberAvailable: {
         type: Boolean,
-        required: true,
         default: false,
     },
     guildMemberRemove: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildMemberUpdate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildScheduledEventCreate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildScheduledEventDelete: {
         type: Boolean,
-        required: true,
         default: true,
     },
     guildScheduledEventUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     guildScheduledEventUserAdd: {
         type: Boolean,
-        required: true,
         default: false,
     },
     guildScheduledEventUserRemove: {
         type: Boolean,
-        required: true,
         default: false,
     },
     guildUpdate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     integrationCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     inviteCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     inviteDelete: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageDeleteBulk: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageReactionAdd: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageReactionRemove: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageReactionRemoveAll: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageReactionRemoveEmoji: {
         type: Boolean,
-        required: true,
         default: false,
     },
     messageUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     roleCreate: {
         type: Boolean,
-        required: true,
         default: true,
     },
     roleDelete: {
         type: Boolean,
-        required: true,
         default: true,
     },
     roleUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     stageInstanceCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     stageInstanceDelete: {
         type: Boolean,
-        required: true,
         default: false,
     },
     stageInstanceUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     stickerCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     stickerDelete: {
         type: Boolean,
-        required: true,
         default: false,
     },
     stickerUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     threadCreate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     threadDelete: {
         type: Boolean,
-        required: true,
         default: false,
     },
     threadListSync: {
         type: Boolean,
-        required: true,
         default: false,
     },
     threadMemberUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     threadMembersUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     threadUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     typingStart: {
         type: Boolean,
-        required: true,
         default: false,
     },
     userUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     voiceStateUpdate: {
         type: Boolean,
-        required: true,
         default: false,
     },
     webhookUpdate: {
         type: Boolean,
-        required: true,
         default: true,
     },
 });
@@ -331,7 +273,7 @@ const serverSchema = new mongoose_1.default.Schema({
     },
     settings: {
         type: settingsSchema,
-        required: true,
+        default: settingsSchema,
     },
 });
 const ServerModel = mongoose_1.default.model("easylogs-servers", serverSchema, "EasyLogs-Servers");
