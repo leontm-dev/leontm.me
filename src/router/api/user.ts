61438f3305bf5d4e6e834041204d07a05abb329b
LeonTM <lanneken09@gmail.com>
2024-08-07 17:17:04 +0200

    Updated urls and methods

--- src/router/api/user.ts
+++ src/router/api/user.ts
@@ -6,24 +6,20 @@ import express from "express";
 
 import {
   getAllUsers,
-  getUserById,
-  getUserBySessionToken,
-  getUserByUsername,
+  getUser,
   deleteUser,
+  updateUserServices,
   updateUserAuth,
   updateUserInfo,
-  updateUserServices,
 } from "../../controller/user";
 
 // Code
 
 export default (router: express.Router) => {
   router.get("/users/all", getAllUsers);
-  router.get("/users/get?id=", getUserById);
-  router.get("/users/get?username=", getUserByUsername);
-  router.get("/users/get?sessionToken=", getUserBySessionToken);
+  router.get("/users/get", getUser);
+  router.put("/users/services/update?id=", updateUserServices);
   router.put("/users/auth/update?id=", updateUserAuth);
   router.put("/users/info/update?id=", updateUserInfo);
-  router.put("/users/services/update?id=", updateUserServices);
   router.delete("/users/delete?id=", deleteUser);
 };
