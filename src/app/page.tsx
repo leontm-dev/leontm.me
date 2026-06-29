def902e506af9c84b4c8095dd59d72687350f7f6
Leon <LAnneken09@gmail.com>
2026-06-29 15:20:10 +0200

    Updated skills and fixed the layout a little

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { PageFooter } from "@/components/outwards/footer";
 import { FontChangingText } from "@/components/font-changing-text";
 import { MainSidebar } from "@/components/main-sidebar";
@@ -6,16 +7,27 @@ import GradientBlinds from "@/components/GradientBlinds";
 import { ListIcon } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import { ProfileSkill } from "./frontPage/skill";
-import { SiJavascript, SiTypescript } from "react-icons/si";
+import {
+  SiExpress,
+  SiMongodb,
+  SiNestjs,
+  SiNextdotjs,
+  SiReact,
+  SiTypescript,
+} from "react-icons/si";
+import { useTheme } from "next-themes";
 
 export default function Page() {
+  const { resolvedTheme } = useTheme();
   return (
-    <div className="w-screen relative flex flex-col gap-4 justify-center">
-      <div className="h-screen w-full flex flex-col items-center justify-center">
-        <FontChangingText text="LeonTM" className="text-[200px] absolute" />
+    <div className="relative flex w-screen flex-col items-center justify-center gap-4">
+      <div className="flex h-screen w-full flex-col items-center justify-center">
+        <FontChangingText text="LeonTM" className="absolute text-[200px]" />
         <GradientBlinds
           className="h-screen"
-          gradientColors={["#e7000b", "#fe707c"]}
+          gradientColors={
+            resolvedTheme === "dark" ? ["#e7000b", "#fe707c"] : ["#e7000b"]
+          }
           angle={30}
           noise={0.3}
           blindCount={20}
@@ -37,9 +49,8 @@ export default function Page() {
         />
       </div>
       <MainSidebar />
-
-      <div className="w-full flex flex-col items-center justify-center">
-        <div className="flex flex-row items-center m-2 justify-evenly w-5/6">
+      <div className="flex w-full flex-col items-center justify-center">
+        <div className="m-2 flex w-5/6 flex-row items-center justify-evenly">
           <Image
             src={"/profilePicture.png"}
             className="aspect-square"
@@ -47,17 +58,39 @@ export default function Page() {
             width={256}
             alt=""
           />
-          <div className="flex flex-col items-start justify-center">
-            <h1 className="font-dancing-script-variable text-2xl">Tach!</h1>
+          <div className="flex flex-col items-start justify-center gap-2">
+            <h2 className="font-dancing-script-variable text-3xl">Tach!</h2>
             <p>
-              I am Leon, a German student that loves programming. <br /> Some of
-              my skills:
+              I am Leon, a German student that loves programming and automating
+              repetitive tasks in my daily life.
             </p>
-            <div className="flex flex-row items-center gap-1 flex-wrap">
-              <ProfileSkill
-                icon={<SiTypescript className="text-[#3178C6]" />}
-                text="TypeScript"
-              />
+            <div className="flex flex-col gap-1">
+              <p>Some of my skills: </p>
+              <div className="flex flex-row flex-wrap items-center gap-1">
+                <ProfileSkill
+                  icon={<SiTypescript className="text-[#3178C6]" />}
+                  text="TypeScript"
+                />
+                <ProfileSkill
+                  icon={<SiNestjs className="text-[#EA2845]" />}
+                  text="NestJS"
+                />
+                <ProfileSkill
+                  icon={<SiMongodb className="text-[#00ED64]" />}
+                  text="MongoDB"
+                />
+                <ProfileSkill
+                  icon={
+                    <SiNextdotjs className="text-[#171717] dark:text-white" />
+                  }
+                  text="Next.js"
+                />
+                <ProfileSkill icon={<SiExpress />} text="Express" />
+                <ProfileSkill
+                  icon={<SiReact className="text-[#58C4DC]" />}
+                  text="React"
+                />
+              </div>
             </div>
           </div>
         </div>
